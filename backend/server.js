1cadc1f9bf22f5ed65e47d82a110a27beed951ef
habib610 <habib54562@gmail.com>
2021-03-30 22:14:08 +0600

    fresh-starting

--- backend/server.js
+++ backend/server.js
@@ -30,11 +30,7 @@ connectionDB()
 //     })
 // })
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);
+
 
 app.use('/api/trips',tripsRouter)
 app.use('/api/users', userRouter)
